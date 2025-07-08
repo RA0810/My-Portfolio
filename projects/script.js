d7dab801a93e173f41585ce636d04120efb68e8c
RA0810 <115402855+RA0810@users.noreply.github.com>
2025-07-08 15:03:05 +0530

    Improve responsiveness and polish layout for mobile/laptop; ensure home section and images are optimized

--- projects/script.js
+++ projects/script.js
@@ -43,6 +43,24 @@ function getProjects() {
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
+        <div class="grid-item ${project.category}">
+        <div class="box tilt" style="width: 380px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`
+    });
     projects.forEach(project => {
         projectsHTML += `
         <div class="grid-item ${project.category}">
@@ -51,6 +69,7 @@ function showProjects(projects) {
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
+        ${project.id ? `<span style='font-size:1.1em;color:#ffd900;font-weight:600;'>ID: ${project.id}</span>` : ''}
         </div>
         <div class="desc">
           <p>${project.desc}</p>
